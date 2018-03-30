5080d199c9ebf78c40a4ecc517730acd32656081
cherchok <viktor.yurzanov@gmail.com>
2018-03-30 19:37:31 +0300

    module 4 task 1

--- src/main/java/javase04/t01/JavaKeyWords.java
+++ src/main/java/javase04/t01/JavaKeyWords.java
@@ -6,8 +6,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class JavaKeyWords {
-    private int comments;
-    private int quotes;
+    private static int comments;
+    private static int quotes;
     private static final String[] WORDS_LIST = new String[]{
             "abstract", "continue", "for", "new", "switch", "assert", "default", "goto*package", "synchronized",
             "boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw", "byte",
@@ -15,33 +15,66 @@ public class JavaKeyWords {
             "extends", "int", "short", "try", "char", "final", "interface", "static", "void", "class", "finally",
             "long", "strictfp", "volatile", "const", "float", "native", "super", "while"
     };
-    private Map<String, KeyWordsCounter> wordsInFile = new HashMap<>();
+    private static Map<String, KeyWordsCounter> wordsInFile = new HashMap<>();
 
     public void wordSplitter(File fin, File out) throws IOException {
 
     }
 
     private static void parseWord(String[] word) {
-        if (word.length >= 0) {
-            for (int i = 0; i < word.length; i++) {
-                if (word[i].contains("\\")) {
-                    break;
-                }
-                if (word[i].length() > 0) {
-
+        for (String aWord : word) {
+            if (aWord.contains("\\")) {
+                break;
+            }
+            if (aWord.length() > 0) {
+                if (comments > 0 || quotes > 0) {
+                    if (comments > 0) {
+                        commentsCounter(aWord);
+                    } else {
+                        if (aWord.length() > 1) {
+                            if (aWord.charAt(aWord.length() - 1) == '"' &&
+                                    aWord.charAt(aWord.length() - 2) == '\\') {
+                                quotes--;
+                            } else if (aWord.charAt(aWord.length() - 1) == '"') {
+                                quotes--;
+                            }
+                        }
+                    }
+                } else {
+                    if (aWord.contains("*/")) {
+                        commentsCounter(aWord);
+                    } else {
+                        if (aWord.charAt(0) == '"') {
+                            quotes++;
+                            if (aWord.length() > 1 && aWord.charAt(aWord.length() - 1) == '"') {
+                                quotes--;
+                            }
+                        } else {
+                            for (String keyword : WORDS_LIST) {
+                                if (keyword.equals(aWord)) {
+                                    if (wordsInFile.containsKey(keyword)) {
+                                        wordsInFile.get(keyword).increment();
+                                    } else {
+                                        wordsInFile.put(keyword, new KeyWordsCounter());
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
 
     }
 
-
-    private void commmentsCounter(String string) {
+    private static void commentsCounter(String string) {
         comments++;
         if (string.contains("/*")) {
             comments = 0;
         }
 
     }
+
+
 
 }
