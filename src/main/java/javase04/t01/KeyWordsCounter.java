c3cacb2db8e55339fcd97ffb839040855fb50549
cherchok <viktor.yurzanov@gmail.com>
2018-03-30 15:40:49 +0300

    module 4 task 1

---
+++ src/main/java/javase04/t01/KeyWordsCounter.java
@@ -0,0 +1,17 @@
+package javase04.t01;
+
+public class KeyWordsCounter {
+    private int count;
+
+    KeyWordsCounter() {
+        count = 0;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void increment(){
+        count++;
+    }
+}
